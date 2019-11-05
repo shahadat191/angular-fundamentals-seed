109357383701b3e07ba33e22a09f07247e72f1a5
Mohammad Shahadath Hossen <shahadath.hossen@orbitax.com>
2019-11-05 09:37:14 +0600

    Update

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <passenger-viewer></passenger-viewer>
     </div>
   `
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
